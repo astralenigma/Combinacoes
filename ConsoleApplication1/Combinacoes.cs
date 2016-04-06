2ca224f5e2db6aa42b6be1f4a37213468635de95
Rui Neves Carvalho <astralenigma@hotmail.com>
2016-04-06 23:44:28 +0100

    Inicializção deste lado.

---
+++ ConsoleApplication1/Combinacoes.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combinacoes
+{
+    class CombinacoesDeIngredientes : List<String>
+    {
+        public override String ToString()
+        {
+            String list = "";
+            foreach (String comb in this)
+            {
+                list += comb + "\n";
+            }
+            return list;
+        }
+
+        public CombinacoesDeIngredientes removerConhecidas(CombinacoesDeIngredientes conhecidas)
+        {
+            CombinacoesDeIngredientes combsX= new CombinacoesDeIngredientes();
+            combsX.AddRange(this.Except(conhecidas));
+            return combsX;
+        }
+
+        public CombinacoesDeIngredientes adicionarCombinacoes(CombinacoesDeIngredientes novasCombin)
+        {
+            CombinacoesDeIngredientes uniao = new CombinacoesDeIngredientes();
+            IEnumerable<String> union = this.Union<String>(novasCombin).Distinct();
+            uniao.AddRange(union);
+            
+            //foreach (String comb in union)
+            //{
+            //    uniao.Add(comb);
+            //}
+            return uniao;
+        }
+        public String contarComposicao()
+        {
+            ListaIngredientes lista = new ListaIngredientes();
+            string sOutput = "";
+            foreach (String comb in this)
+            {
+                lista.receberListaDeIngredientes(comb);
+
+            }
+            lista.Sort();
+            String lastIng = "";
+            int countIng = 0;
+            foreach (String ingCount in lista)
+            {
+                if (lastIng.CompareTo(ingCount) == 0)
+                {
+                    countIng++;
+                }
+                else
+                {
+                    if (lastIng != "")
+                    {
+                        sOutput += countIng + " ";
+                    }
+                    countIng = 0;
+                    sOutput += ingCount + " ";
+                    lastIng = ingCount;
+                }
+            }
+            sOutput += countIng;
+            return sOutput;
+        }
+        public void combinarMaisUm(String segundaParte)
+        {
+            CombinacoesDeIngredientes novas = new CombinacoesDeIngredientes();
+            foreach (string comb in this)
+            {
+                ListaIngredientes liComb = ListaIngredientes.criarListaDeIngredientes(comb);
+                if (!liComb.Contains(segundaParte))
+                {
+                    liComb.Add(segundaParte);
+                    liComb.Sort();
+                    novas.Add(liComb.ToString());
+                }
+            }
+            this.Add(segundaParte);
+            this.AddRange(novas);
+        }
+        
+        public CombinacoesDeIngredientes combinarReceita(String segundaReceita)
+        {
+            ListaIngredientes listaReceita = ListaIngredientes.criarListaDeIngredientes(segundaReceita);
+            CombinacoesDeIngredientes novas = new CombinacoesDeIngredientes();
+            foreach (string comb in this)
+            {
+                ListaIngredientes liComb = ListaIngredientes.criarListaDeIngredientes(comb);
+                foreach (String ingRec in listaReceita)
+                {
+                    liComb.Add(ingRec);
+                    liComb.Sort();
+                }
+                novas.Add(liComb.ToString());
+            }
+            return novas;
+        }
+        
+        public int ContarCombinacoesRestantes()
+        {
+            CombinacoesDeIngredientes contaCombi = this;
+            int count = 0;
+            while (contaCombi.Count!=0)
+            {
+                count++;
+                String gCombinacao = contaCombi.Last();
+                CombinacoesDeIngredientes listaEliminacao = ListaIngredientes.criarListaDeIngredientes(gCombinacao).criarCombinacoesDeDescoberta();
+                contaCombi=contaCombi.removerConhecidas(listaEliminacao);
+            }
+            return count;
+        }
+
+        public CombinacoesDeIngredientes listarCombinacoesNecessarias()
+        {
+            CombinacoesDeIngredientes cdi = new CombinacoesDeIngredientes();
+            CombinacoesDeIngredientes contaCombi = this;
+            int count = 0;
+            while (contaCombi.Count != 0)
+            {
+                count++;
+                String gCombinacao = contaCombi.Last();
+                cdi.Add(gCombinacao);
+                CombinacoesDeIngredientes listaEliminacao = ListaIngredientes.criarListaDeIngredientes(gCombinacao).criarCombinacoesDeDescoberta();
+                contaCombi = contaCombi.removerConhecidas(listaEliminacao);
+            }
+            cdi.Reverse();
+            return cdi;
+        }
+    }
+
+}
---
+++ ConsoleApplication1/InterfaceCombi.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combinacoes
+{
+    class InterfaceCombi
+    {
+        static ListaIngredientes listaChems;
+        static CombinacoesDeIngredientes combinacoes3Elementos;
+        static CombinacoesDeIngredientes combinacoesDescoberta;
+        static CombinacoesDeIngredientes combinacoesEncontradas;
+        static CombinacoesDeIngredientes combinacoesTestadas;
+
+        private static CombinacoesDeIngredientes calcularListaDescoberta(ListaIngredientes listaChems, CombinacoesDeIngredientes combinacoesEncontradas, CombinacoesDeIngredientes combinacoesTestadas)
+        {
+            Console.WriteLine(combinacoesDescoberta.Count);
+            combinacoesDescoberta = CombinacoesDescobertaTeste(listaChems, combinacoesDescoberta, combinacoesEncontradas);
+            combinacoesDescoberta = reordenarCombinacoes(combinacoesDescoberta);
+            combinacoesDescoberta = combinacoesDescoberta.removerConhecidas(combinacoesTestadas);
+            combinacoesDescoberta = combinacoesDescoberta.removerConhecidas(combinacoesEncontradas);
+            Console.WriteLine(combinacoesDescoberta.Count);
+            Console.WriteLine(combinacoesTestadas.Count);
+            return combinacoesDescoberta;
+        }
+
+        public static void processarInformacao()
+        {
+            calcularListaDescoberta(listaChems, combinacoesEncontradas, combinacoesTestadas);
+            Console.WriteLine(combinacoesDescoberta.ContarCombinacoesRestantes());
+        }
+
+        //public static void processarIngrediente(String ingrediente)
+        //{
+        //    calcularListaDescobertaR(listaChems, combinacoesEncontradas, combinacoesTestadas, ingrediente);
+        //}
+
+        private static CombinacoesDeIngredientes CombinacoesDescobertaTeste(ListaIngredientes listaChems, CombinacoesDeIngredientes combinacoesDescoberta, CombinacoesDeIngredientes combinacoesEncontradas)
+        {
+            foreach (String comb in combinacoesEncontradas)
+            {
+                combinacoesDescoberta = combinacoesDescoberta.removerConhecidas(listaChems.criarCombinacoesExclusivas(comb));
+                Console.WriteLine(combinacoesDescoberta.Count);
+            }
+            return combinacoesDescoberta;
+        }
+
+        private static void calcularListaDescobertaR(ListaIngredientes listaChems, CombinacoesDeIngredientes combinacoesEncontradas, CombinacoesDeIngredientes combinacoesTestadas, String ingrediente)
+        {
+            Console.WriteLine();
+            Console.WriteLine(ingrediente);
+            CombinacoesDeIngredientes combinacoesDoIngrediente = listaChems.criarCombinacoesExclusivas(ingrediente);
+            CombinacoesDeIngredientes combinacoesDescobertaR = calcularListaDescoberta(listaChems, combinacoesEncontradas, combinacoesTestadas);
+            listarReceitas(combinacoesDescobertaR);
+        }
+
+        private static void listarReceitas(CombinacoesDeIngredientes combinacoesDescoberta)
+        {
+            Console.WriteLine(combinacoesDescoberta.Last());
+            Console.WriteLine(combinacoesDescoberta.ElementAt((combinacoesDescoberta.Count - 2)));
+            Console.WriteLine(combinacoesDescoberta.ElementAt((combinacoesDescoberta.Count - 3)));
+            Console.WriteLine(combinacoesDescoberta.ElementAt((combinacoesDescoberta.Count - 4)));
+            Console.WriteLine(combinacoesDescoberta.ElementAt((combinacoesDescoberta.Count - 5)));
+            Console.WriteLine(combinacoesDescoberta.First());
+            Console.WriteLine(combinacoesDescoberta.ElementAt((1)));
+            Console.WriteLine(combinacoesDescoberta.ElementAt((2)));
+            Console.WriteLine(combinacoesDescoberta.ElementAt((3)));
+            Console.WriteLine(combinacoesDescoberta.ElementAt((4)));
+        }
+
+        private static void listarCalculoListaDescobertaR(ListaIngredientes listaChems, CombinacoesDeIngredientes combinacoesEncontradas, CombinacoesDeIngredientes combinacoesTestadas)
+        {
+            foreach (String item in listaChems)
+            {
+                calcularListaDescobertaR(listaChems, combinacoesEncontradas, combinacoesTestadas, item);
+            }
+        }
+
+        public static void lerFicheiro()
+        {
+            listaChems = new ListaIngredientes();
+            combinacoesEncontradas = new CombinacoesDeIngredientes();
+            CombinacoesDeIngredientes combinacoesTestadasIn = new CombinacoesDeIngredientes();
+
+            string[] lines = System.IO.File.ReadAllLines(@"input.txt");
+            int sinal = 0;
+
+            foreach (string line in lines)
+            {
+                switch (line)
+                {
+                    case "-": sinal = 1;
+                        break;
+                    case "--": sinal = 2;
+                        break;
+                    case "---": sinal = 3;
+                        break;
+                    case "----":
+                    case "-----": sinal = 4;
+                        break;
+                    default:
+                        switch (sinal)
+                        {
+                            case 1:
+                                listaChems.Add(line);
+                                break;
+                            case 2:
+                                combinacoesEncontradas.Add(line);
+                                break;
+                            case 3: combinacoesTestadasIn.Add(line);
+                                break;
+                            default: Console.WriteLine("Erro");
+                                break;
+                        }
+                        break;
+                }
+            }
+            combinacoesTestadas = listarCombinacoesFalhadas(combinacoesTestadasIn);
+            combinacoesDescoberta = listaChems.criarCombinacoesDeDescoberta();
+            combinacoesDescoberta.Sort((a, b) => b.Length.CompareTo(a.Length));
+            //Remover necessidade de Contar as testadas.
+            consolaOutput(combinacoesTestadasIn);
+
+        }
+
+        private static void consolaOutput(CombinacoesDeIngredientes combinacoesTestadasIn)
+        {
+            Console.WriteLine(listaChems);
+            Console.WriteLine(combinacoesEncontradas);
+            Console.WriteLine(combinacoesTestadasIn);
+            Console.WriteLine(combinacoesTestadas.Count);
+        }
+
+        public static void escreverFicheiro()
+        {
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(@"output.txt"))
+            {
+
+                file.WriteLine("-");
+                foreach (String item in listaChems)
+                {
+                    file.WriteLine(item);
+                }
+                file.WriteLine("--");
+                foreach (String item in combinacoesEncontradas)
+                {
+                    file.WriteLine(item);
+                }
+                file.WriteLine("---");
+                foreach (String item in combinacoesTestadas)
+                {
+                    file.WriteLine(item);
+                }
+                file.WriteLine("----");
+
+                foreach (String item in combinacoesDescoberta.listarCombinacoesNecessarias())
+                {
+                    file.WriteLine(item);
+                }
+                file.WriteLine("-----");
+                file.WriteLine("You need to make at least "+combinacoesDescoberta.ContarCombinacoesRestantes()+" combinations to make sure there are no more recipes.");
+            }
+        }
+
+        private static CombinacoesDeIngredientes listarCombinacoesFalhadas(CombinacoesDeIngredientes combs)
+        {
+            CombinacoesDeIngredientes combsF = new CombinacoesDeIngredientes();
+            foreach (String item in combs)
+            {
+                ListaIngredientes LI = ListaIngredientes.criarListaDeIngredientes(item);
+                combsF.AddRange(LI.criarCombinacoesDeDescoberta());
+            }
+            CombinacoesDeIngredientes combsOut = new CombinacoesDeIngredientes();
+            combsOut.AddRange(combsF.Distinct());
+            combsOut.Sort();
+            return combsOut;
+        }
+
+        private static CombinacoesDeIngredientes reordenarCombinacoes(CombinacoesDeIngredientes combinacoesDescoberta)
+        {
+            List<String> combs = combinacoesDescoberta.OrderBy(x => x.Length).ToList();
+            CombinacoesDeIngredientes combsOut = new CombinacoesDeIngredientes();
+            foreach (String item in combs)
+            {
+                combsOut.Add(item);
+            }
+            return combsOut;
+        }
+
+
+    }
+}
